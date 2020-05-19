c5a5c04a7e1660a83ba341e5e91d604378e0246d
Adrian Marciniuk <marciniuk.adrian123@gmail.com>
2020-05-19 15:43:24 +0200

    nowa wersja

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,8 +1,8 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "0b8e0a2db0802d6336b4a3ed3cbfef10",
-"/": "0b8e0a2db0802d6336b4a3ed3cbfef10",
+  "index.html": "20e5b788ee4e9a01f87a2578320a0e4f",
+"/": "20e5b788ee4e9a01f87a2578320a0e4f",
 "assets/assets/private_chat_wallpaper.png": "79a300713eb748518781489ba2458832",
 "assets/assets/new_group_wallpaper.png": "da27dd53626f515a565b023c9690b4c6",
 "assets/assets/fluffychat-banner.png": "c79eb543ae8dc4af5c21dedd92fc5dce",
@@ -13,13 +13,15 @@ const RESOURCES = {
 "assets/assets/logo.png": "3ac2c2c7746ce2a7ac304052ea7d09ee",
 "assets/assets/js/package/olm.js": "b63a3b8c4c4735266e626a3cba287657",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/LICENSE": "7188804c14598af16fab1fa7fa3154be",
+"assets/LICENSE": "ac324f9dcc185a79b3dfb685bba467c8",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/FontManifest.json": "f7161631e25fbd47f3180eae84053a51",
 "assets/AssetManifest.json": "39ae6143a86017b675b2aec568b2cab8",
+"sql-wasm.js": "7ce6f6c33529a9703e33e194e79b7691",
 "browserconfig.xml": "653d077300a12f09a69caeea7a8947f8",
 "manifest.json": "d0730d8ba1844e2657a61320ef2ba6cc",
-"main.dart.js": "671f183d53ef68898609212416db53a8"
+"main.dart.js": "18dbd448d2b9ded326a56dacd3ab0b73",
+"sql-wasm.wasm": "9acb7a2acdb22234cf19be0aa2f332d0"
 };
 
 self.addEventListener('activate', function (event) {
